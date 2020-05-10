37465c39d5fee7e08746c071613d70d24d90aea1
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-05-10 13:55:41 +0300

    fixed

--- download.sh
+++ download.sh
@@ -2,4 +2,4 @@
 mkdir -p pretrained
 wget -O ./pretraned/ https://storage.googleapis.com/bert_models/2018_11_23/multi_cased_L-12_H-768_A-12.zip
 # Loading data
-git clone git@bitbucket.org:nicolay_r/bert-rusentrel-experiments.git data/
+git clone https://nicolay_r@bitbucket.org/nicolay_r/bert-rusentrel-experiments.git
