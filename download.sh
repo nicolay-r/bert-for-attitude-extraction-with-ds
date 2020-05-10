77abf8b9ac1de0f10629fa2c101d14edf2f8cd56
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-05-10 14:02:57 +0300

    fixed, upd script

--- download.sh
+++ download.sh
@@ -1,5 +1,9 @@
 # Loading model
-mkdir -p pretrained
-wget -O ./pretraned/ https://storage.googleapis.com/bert_models/2018_11_23/multi_cased_L-12_H-768_A-12.zip
+fzip=multi_cased_L-12_H-768_A-12.zip
+subdir=pretrained
+mkdir -p $subdir
+wget https://storage.googleapis.com/bert_models/2018_11_23/$fzip -O ./$subdir/$fzip
+cd ./$subdir/ && unzip $fzip -d .
+
 # Loading data
 git clone https://nicolay_r@bitbucket.org/nicolay_r/bert-rusentrel-experiments.git
