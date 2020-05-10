6d7711b756c2d84785fc3ecbbcafef82474f399f
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-05-10 20:46:03 +0300

    upd

--- setup.sh
+++ setup.sh
@@ -10,3 +10,13 @@ pip install -r requirements.txt
 # important to fix this issue: https://github.com/tensorflow/tensorflow/issues/32949
 pip install gast==0.2.2
 
+# Install CUDA
+sudo apt install nvidia-cuda-toolkit
+
+# How to replace libcublas
+# find /usr/lib -name "libcublas.so.9.1"
+# ln -s /usr/lib/x86_64-linux-gnu/libcublas.so.9.1 /usr/lib/x86_64-linux-gnu/libcublas.so.9.0
+# ln -s /usr/lib/x86_64-linux-gnu/libcusolver.so.9.1 /usr/lib/x86_64-linux-gnu/libcusolver.so.9.0
+# ln -s /usr/lib/x86_64-linux-gnu/libcudart.so.9.1 /usr/lib/x86_64-linux-gnu/libcudart.so.9.0
+# ln -s /usr/lib/x86_64-linux-gnu/libcufft.so.9.1 /usr/lib/x86_64-linux-gnu/libcufft.so.9.0
+# ln -s /usr/lib/x86_64-linux-gnu/libcurand.so.9.1 /usr/lib/x86_64-linux-gnu/libcurand.so.9.0
