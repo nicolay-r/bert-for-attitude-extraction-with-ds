e999b945e4e2b8b3ab44afd3c865a81877e808d1
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-05-26 22:36:45 +0300

    upd

---
+++ _run_3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+########################################
+# Run all the task on a single Videocard
+########################################
+
+
+source _models.sh
+
+for i in $s3; do
+
+    OLDIFS=$IFS
+    IFS=','
+
+    # Split into
+    # $1 -- folder,
+    # $2 -- task_name
+    set -- $i;
+
+    folder=$1
+    task_name=$2
+
+    src=./data/$1
+
+    ./run_classifier.sh 0 $folder $task_name
+
+done;
+
+IFS=$OLDIFS
