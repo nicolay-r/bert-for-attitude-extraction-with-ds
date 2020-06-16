20074d2b294c9fc3bb05c049d0c2e211ed34e566
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-06-16 23:39:42 +0300

    added test run

---
+++ _test_run.sh
@@ -0,0 +1,21 @@
+
+for i in $m1; do
+
+    OLDIFS=$IFS
+    IFS=','
+
+    # Split into
+    # $1 -- folder,
+    # $2 -- task_name
+    set -- $i;
+
+    folder=$1
+    task_name=$2
+
+    src=./data/$1
+
+    ./run_classifier.sh 0 $folder $task_name
+
+done;
+
+IFS=$OLDIFS
