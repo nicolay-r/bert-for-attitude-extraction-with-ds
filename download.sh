50c5a9337221a5615331c0bf4fad5f379f0026a6
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-07-16 15:42:54 +0300

    upd

--- download.sh
+++ download.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Loading model
-fzip=multi_cased_L-12_H-768_A-12.zip
 subdir=pretrained
 mkdir -p $subdir
-wget https://storage.googleapis.com/bert_models/2018_11_23/$fzip -O ./$subdir/$fzip
-cd ./$subdir/ && unzip $fzip -d .
+
+# BERT-Base Multilingual model
+bert_base=multi_cased_L-12_H-768_A-12.zip
+wget https://storage.googleapis.com/bert_models/2018_11_23/$bert_base -O ./$subdir/$bert_base
+cd ./$subdir/ && unzip $bert_base -d .
+
+# RuBert (IPavlov)
+rubert_base=rubert_cased_L-12_H-768_A-12.tar.gz
+wget http://files.deeppavlov.ai/deeppavlov_data/bert/$rubert_base -O ./$subdir/$rubert_base
+cd ./$subdir/ && tar -xvf $rubert_base
