4e87d99111bb125ef0c674bfcdb40e5b6bec4753
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-07-16 12:17:27 +0300

    updated collection

--- data_download.sh
+++ data_download.sh
@@ -3,6 +3,6 @@
 # Loading data
 data=data.zip
 # no-cv data
-curl -L -o $data https://www.dropbox.com/s/1zg6w2xiym8ywz5/bert-train-fixed-data-1.1.zip?dl=1
+curl -L -o $data https://www.dropbox.com/s/wmkgqvmmho5hvyn/bert-experiments.1.4.zip?dl=1
 mkdir -p data
 unzip $data -d data/.
