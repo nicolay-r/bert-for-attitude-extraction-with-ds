a5342146379ecd2bf7c36ffa653181a305ec1338
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-02-20 07:22:26 +0000

    SETTED UP: Storing only a last state in history

--- run_classifier.py
+++ run_classifier.py
@@ -662,6 +662,7 @@ def main(_):
       cluster=tpu_cluster_resolver,
       master=FLAGS.master,
       model_dir=FLAGS.output_dir,
+      keep_checkpoint_max=1,
       save_checkpoints_steps=FLAGS.save_checkpoints_steps,
       tpu_config=tf.contrib.tpu.TPUConfig(
           iterations_per_loop=FLAGS.iterations_per_loop,
