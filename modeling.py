d323100fcac3a3e971812ab85478d911c8782efa
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-03-28 15:15:53 +0000

    Rollback to the default distance feature

--- modeling.py
+++ modeling.py
@@ -497,34 +497,68 @@ def embedding_postprocessor(input_tensor,
       full_position_embeddings = tf.get_variable(
           name=position_embedding_name,
           shape=[max_position_embeddings, width],
-          initializer=create_initializer(initializer_range),
-          dtype=tf.float32)
+          initializer=create_initializer(initializer_range))
       # Since the position embedding table is a learned variable, we create it
       # using a (long) sequence length `max_position_embeddings`. The actual
       # sequence length might be shorter than this, for faster training of
       # tasks that do not have long sequences.
-      sliced = tf.slice(full_position_embeddings, [0, 0], [seq_length, -1])
-
-      if position_ids is not None:
-        position_embeddings = reorder_elements_in_input(
-          sliced=sliced,
-          inds=position_ids,
-          batch_size=batch_size,
-          handler=sliced_reordering)
-      else:
-        # So `full_position_embeddings` is effectively an embedding table
-        # for position [0, 1, 2, ..., max_position_embeddings-1], and the current
-        # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
-        # perform a slice.
-        position_embeddings = sliced
-
+      #
+      # So `full_position_embeddings` is effectively an embedding table
+      # for position [0, 1, 2, ..., max_position_embeddings-1], and the current
+      # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
+      # perform a slice.
+      position_embeddings = tf.slice(full_position_embeddings, [0, 0],
+                                     [seq_length, -1])
+      num_dims = len(output.shape.as_list())
+  
+      # Only the last two dimensions are relevant (`seq_length` and `width`), so
+      # we broadcast among the first dimensions, which is typically just
+      # the batch size.
+      position_broadcast_shape = []
+      for _ in range(num_dims - 2):
+        position_broadcast_shape.append(1)
+      position_broadcast_shape.extend([seq_length, width])
       position_embeddings = tf.reshape(position_embeddings,
-                                       [batch_size, seq_length, width])
-
+                                       position_broadcast_shape)
       output += position_embeddings
-
+  
   output = layer_norm_and_dropout(output, dropout_prob)
   return output
+
+  # if use_position_embeddings:
+  #   assert_op = tf.assert_less_equal(seq_length, max_position_embeddings)
+  #   with tf.control_dependencies([assert_op]):
+  #     full_position_embeddings = tf.get_variable(
+  #         name=position_embedding_name,
+  #         shape=[max_position_embeddings, width],
+  #         initializer=create_initializer(initializer_range),
+  #         dtype=tf.float32)
+  #     # Since the position embedding table is a learned variable, we create it
+  #     # using a (long) sequence length `max_position_embeddings`. The actual
+  #     # sequence length might be shorter than this, for faster training of
+  #     # tasks that do not have long sequences.
+  #     sliced = tf.slice(full_position_embeddings, [0, 0], [seq_length, -1])
+
+  #     if position_ids is not None:
+  #       position_embeddings = reorder_elements_in_input(
+  #         sliced=sliced,
+  #         inds=position_ids,
+  #         batch_size=batch_size,
+  #         handler=sliced_reordering)
+  #     else:
+  #       # So `full_position_embeddings` is effectively an embedding table
+  #       # for position [0, 1, 2, ..., max_position_embeddings-1], and the current
+  #       # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
+  #       # perform a slice.
+  #       position_embeddings = sliced
+
+  #     position_embeddings = tf.reshape(position_embeddings,
+  #                                      [batch_size, seq_length, width])
+
+  #     output += position_embeddings
+
+  # output = layer_norm_and_dropout(output, dropout_prob)
+  # return output
 
 
 def reorder_elements_in_input(sliced, inds, handler, batch_size):
